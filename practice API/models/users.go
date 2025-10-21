6ee24181b3b57a2975b5650fe0723cc2a2032402
zakarie21 <zakarieomar43@outlook.com>
2025-10-21 23:56:53 +0100

    signup updates for API

--- practice API/models/users.go
+++ practice API/models/users.go
@@ -2,21 +2,30 @@ package models
 
 import (
 	"RESTApi/db"
+	"RESTApi/utils"
 	"errors"
 )
 
 type Users struct {
 	ID       int    `json:"id"`
 	Email    string `json:"email" binding: "required"`
-	Password string `json:"email" binding: "required"`
+	Password string `json:"password" binding: "required"`
 }
 
 func (u *Users) Save() error {
 	insertQuery:= `INSERT INTO users (email, password) VALUES (?,?)`
-	_, err := db.DB.Exec(insertQuery, u.Email, u.Password)
+	HashedPassword, err:= utils.HashPassword(u.Password)
+	if err != nil{
+		return err
+	}
+
+	_, err = db.DB.Exec(insertQuery, u.Email, HashedPassword)
 	if err != nil {
 		return errors.New("unable to create table")
 	}
 
 	return nil
+
 }
+
+
--- practice API/utils/utils.go
+++ practice API/utils/utils.go
@@ -1,2 +1,14 @@
 package utils
 
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	
+	userPassword,err := bcrypt.GenerateFromPassword([]byte(password), 8)
+	if err != nil{
+		return "", err
+	}
+
+	return string(userPassword), nil
+}
+
